913c9bdf6fb9707f376be78ece8beb8a153ab2af
Elian Avalos <elianemanuelstudy@gmail.com>
2022-12-31 15:05:44 -0300

    Revert "Fetching by region"
    This reverts commit d59f6a7df570cf7b1d8d2c8092abdf4dacba1a3f.

--- src/components/filters/filter.component.tsx
+++ src/components/filters/filter.component.tsx
@@ -1,28 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
-import { ChangeEvent, useState } from "react";
-import { getCountriesByRegion } from "../restCountries.api";
-
 const Filter = () => {
-  const [region, setRegion] = useState("");
-
-  const { refetch } = useQuery(
-    ["countries"],
-    async () => {
-      return (await getCountriesByRegion(region)).data;
-    },
-    {
-      enabled: !!region,
-      refetchOnWindowFocus: false,
-    }
-  );
-
-  const handleSelect = (e: ChangeEvent) => {
-    const $select = e.target as HTMLSelectElement;
-    const region = $select.value;
-    if (!region) return;
-    setRegion(region);
-    refetch();
-  };
   return (
     <div>
       <form>
@@ -30,8 +6,8 @@ const Filter = () => {
           <div>:b</div>
           <input type="text" placeholder="Search for a country" />
         </div>
-        <select name="region" id="region" onChange={handleSelect}>
-          <option>Select by region</option>
+        <select name="region" id="region">
+          <option value="">Select by region</option>
           <option value="africa">Africa</option>
           <option value="america">America</option>
           <option value="asia">Asia</option>
