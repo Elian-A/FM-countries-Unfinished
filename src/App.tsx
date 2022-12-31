2bd3b2b7b6a8b800828995a0f3bb0489454425ac
Elian Avalos <elianemanuelstudy@gmail.com>
2022-12-31 08:41:30 -0300

    Erased unused div

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,8 @@ import Filter from "./components/filters/filter.component";
 function App() {
   return (
     <main>
-      <div>
-        <Filter />
-        <CountriesList />
-      </div>
+      <Filter />
+      <CountriesList />
     </main>
   );
 }
