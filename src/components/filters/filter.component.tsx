2bb132acdf6ed994f7b760dab165c9fe1112da68
Elian Avalos <elianemanuelstudy@gmail.com>
2023-01-03 19:12:26 -0300

    Added filter state logic

--- src/components/filters/filter.component.tsx
+++ src/components/filters/filter.component.tsx
@@ -2,17 +2,28 @@ import { useFiltersStore } from "../../store/filters.store";
 
 const Filter = () => {
   const updateRegion = useFiltersStore((state) => state.updateRegion);
+  const updateText = useFiltersStore((state) => state.updateText);
 
   const handleRegionChange = (e: React.ChangeEvent) => {
     const select = e.target as HTMLSelectElement;
     updateRegion(select.value);
   };
+
+  const handleTextFilter = (e: React.ChangeEvent) => {
+    const input = e.target as HTMLInputElement;
+    updateText(input.value);
+  };
+
   return (
     <div>
       <form>
         <div>
           <div>:b</div>
-          <input type="text" placeholder="Search for a country" />
+          <input
+            type="text"
+            placeholder="Search for a country"
+            onChange={handleTextFilter}
+          />
         </div>
         <select name="region" id="region" onChange={handleRegionChange}>
           <option value="">Select by region</option>
