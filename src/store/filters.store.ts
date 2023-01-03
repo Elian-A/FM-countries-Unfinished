86064c91cc2f933a3ac8693151807f45f0b21a04
Elian Avalos <elianemanuelstudy@gmail.com>
2023-01-03 19:11:40 -0300

    Installed and added zustand devtools Installed node types

--- src/store/filters.store.ts
+++ src/store/filters.store.ts
@@ -1,13 +1,20 @@
 import create from "zustand";
+import { mountStoreDevtool } from "simple-zustand-devtools";
 
 interface FilterState {
   region: string;
   text: string;
   updateRegion: (region: string) => void;
+  updateText: (region: string) => void;
 }
 
 export const useFiltersStore = create<FilterState>((set) => ({
   region: "",
   text: "",
-  updateRegion: (region: string) => set((state: any) => ({ region })),
+  updateRegion: (region: string) => set(() => ({ region })),
+  updateText: (text: string) => set(() => ({ text })),
 }));
+
+if (process.env.NODE_ENV === "development") {
+  mountStoreDevtool("Store", useFiltersStore);
+}
