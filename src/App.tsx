e2be36f9e3911ff51d835390a938cf15225f87cc
Elian Avalos <elianemanuelstudy@gmail.com>
2023-01-06 05:29:19 -0300

    Added React router Created Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import Header from "./components/header/header.component";
 import Index from "./components/index/index.component";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Country from "./components/country/country";
+import CountryError from "./components/countryError/countryError";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <p>Nothing here right now</p>,
+  },
+  {
+    path: "/countries",
+    element: <Index />,
+  },
+  {
+    path: "/countries/:country",
+    element: <Country />,
+    errorElement: <CountryError />,
+  },
+]);
 
 function App() {
   return (
     <div className="bg-gray-200">
       <Header />
-      <Index />
+      <RouterProvider router={router} />
     </div>
   );
 }
